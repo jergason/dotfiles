28449611c328c425776aa45c6c640d3803fca6ba
Jamison Dance <hi@jamison.dance>
2023-05-31 10:23:03 -0600

    update nodejs/yarn config

--- install.sh
+++ install.sh
@@ -66,6 +66,8 @@ if get_confirmation "install nvm"; then
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
   nvm install --lts
+  # allow managing yarn versions
+  corepack enable
 fi
 
 if get_confirmation "install fonts"; then
